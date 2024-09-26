466cb984a9655ccb1583ee4a7fef2a2fef295b1c
Rijal Ghodi <rijalghodi10@gmail.com>
2024-09-26 21:43:35 +0700

    buffer option list for 1 second

--- src/features/admin/form-editor/question/OptionList.tsx
+++ src/features/admin/form-editor/question/OptionList.tsx
@@ -66,7 +66,7 @@ export function OptionList({ questionId }: Props) {
   const [explanation, setExplanation] = useState(answerKey?.data.explanation);
   const [debExplanation, cancelDebExplanation] = useDebouncedValue(
     explanation,
-    2000,
+    1000,
   );
 
   useEffect(() => {
